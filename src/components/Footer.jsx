66664ac5ffb8907e4740a021f5041d798d60733d
aloshhen <166549924+aloshhen@users.noreply.github.com>
2026-01-11 22:52:22 +0100

    Add src/components/Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="bg-dark/50 py-8">
+      <div className="container mx-auto px-4 text-center">
+        <p className="text-light/80">
+          © 2024 Truck Transport Company. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  )
+}
